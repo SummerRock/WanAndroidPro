0698603bb50f22be1d37950e3d18b57599c04336
xiayan <xiayandurant@163.com>
2023-06-06 23:43:18 +0800

    viewbinding

--- MyApplication/app/src/main/java/com/example/myapplication/base/fragment/BaseFragment.java
+++ MyApplication/app/src/main/java/com/example/myapplication/base/fragment/BaseFragment.java
@@ -11,20 +11,22 @@ import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.viewbinding.ViewBinding;
 
-public abstract class BaseFragment<V extends ViewModel> extends Fragment {
-    protected View rootView;
+public abstract class BaseFragment<V extends ViewModel, VB extends ViewBinding> extends Fragment {
     protected V viewModel;
 
-    protected abstract int getLayoutId();
+    protected VB binding;
 
     protected abstract Class<V> getViewModelClass();
+
+    protected abstract VB getBinding(LayoutInflater inflater, ViewGroup container);
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        rootView = inflater.inflate(getLayoutId(), container, false);
-        return rootView;
+        binding = getBinding(inflater, container);
+        return binding.getRoot();
     }
 
     @Override
--- MyApplication/app/src/main/java/com/example/myapplication/main/ui/home/HomeFragment.java
+++ MyApplication/app/src/main/java/com/example/myapplication/main/ui/home/HomeFragment.java
@@ -1,29 +1,31 @@
 package com.example.myapplication.main.ui.home;
 
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.lifecycle.Observer;
 
-import com.example.myapplication.R;
 import com.example.myapplication.base.fragment.BaseFragment;
 import com.example.myapplication.base.model.NetworkModel;
+import com.example.myapplication.databinding.FragmentHomeBinding;
 import com.example.myapplication.main.ui.home.model.HomeModelVo;
 
-public class HomeFragment extends BaseFragment<HomeViewModel> {
-    @Override
-    protected int getLayoutId() {
-        return R.layout.fragment_home;
-    }
-
+public class HomeFragment extends BaseFragment<HomeViewModel, FragmentHomeBinding> {
     @Override
     protected Class<HomeViewModel> getViewModelClass() {
         return HomeViewModel.class;
     }
 
     @Override
+    protected FragmentHomeBinding getBinding(LayoutInflater inflater, ViewGroup container) {
+        return FragmentHomeBinding.inflate(inflater, container, false);
+    }
+
+    @Override
     protected void performAction() {
-        final TextView textView = rootView.findViewById(R.id.text_home);
+        final TextView textView = binding.textHome;
         viewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         viewModel.triggerHomeData(0);
         viewModel.getLiveData().observe(getViewLifecycleOwner(), new Observer<NetworkModel<HomeModelVo>>() {
