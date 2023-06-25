cefad3718a2ecafaa99fb5e5f8b7c6d537d3c31a
xiayan <xiayandurant@163.com>
2023-06-25 17:22:26 +0800

    rename to common module

---
+++ MyApplication/app/src/main/java/com/example/myapplication/main/ui/home/view/HomeListAdapter.java
@@ -0,0 +1,52 @@
+package com.example.myapplication.main.ui.home.view;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.R;
+import com.example.myapplication.main.ui.home.model.HomeModelVo;
+
+import java.util.List;
+
+public class HomeListAdapter extends RecyclerView.Adapter<HomeListAdapter.ViewHolder> {
+
+    private List<HomeModelVo.DatasDTO> data;
+
+    public HomeListAdapter(List<HomeModelVo.DatasDTO> data) {
+        this.data = data;
+    }
+
+    public void setData(List<HomeModelVo.DatasDTO> data) {
+        this.data = data;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    // 自定义ViewHolder类
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
