931f848af3252f44c5c670ae9b849ed1eb0d4415
xiayan <xiayandurant@163.com>
2023-07-25 11:01:40 +0800

    添加kotlin插件

--- app/src/main/java/com/example/myapplication/main/ui/mine/MineViewModel.java
+++ app/src/main/java/com/example/myapplication/main/ui/mine/MineViewModel.java
@@ -4,13 +4,19 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.myapplication.base.login.LoginManager;
+
 public class MineViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
     public MineViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("张三");
+        if (LoginManager.Companion.getInstance().isLogin()) {
+            mText.setValue("张三");
+        } else {
+            mText.setValue("未登录");
+        }
     }
 
     public LiveData<String> getText() {
