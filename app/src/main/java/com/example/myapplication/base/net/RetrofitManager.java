c57c2d3e016fe8217ff032ed92e90c421acc3812
xiayan <xiayandurant@163.com>
2023-08-06 18:30:06 +0800

    添加拦截器

--- app/src/main/java/com/example/myapplication/base/net/RetrofitManager.java
+++ app/src/main/java/com/example/myapplication/base/net/RetrofitManager.java
@@ -1,5 +1,6 @@
 package com.example.myapplication.base.net;
 
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -11,9 +12,16 @@ public class RetrofitManager {
     private RetrofitManager() {
         // 私有的构造方法，防止外部实例化
         if (retrofit == null) {
+            // 创建自定义拦截器
+            CookieInterceptor cookieInterceptor = new CookieInterceptor();
+            // 创建 OkHttpClient 实例并添加拦截器
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .addInterceptor(cookieInterceptor) // 添加自定义拦截器
+                    .build();
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(okHttpClient)
                     .build();
         }
     }
